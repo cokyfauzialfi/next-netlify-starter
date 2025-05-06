f241675dc4648a3f13d2e5861efb032b5ad4a6f8
cokyfauzialfi <coky.fauzi.alfi@gmail.com>
2025-05-06 08:49:22 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Carbon Disclosure Network app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <img>cdn_ads_664px.png</img>  
         </p>
       </main>
 
